8dac36b0560935dbfde8f3834208ce8b4a14b711
Alastair Jones <alastair.peter@gmail.com>
2018-06-25 21:35:44 +0100

    update to netstandard 2.0 (plan to support lower standard versions going forward)

--- AutoCli/Cli.cs
+++ AutoCli/Cli.cs
@@ -3,11 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace AutoCli
 {
-    public class Cli
+	public class Cli
     {
 		private readonly List<CliMethod> methods = new List<CliMethod>();
 		private readonly Func<Type, object> serviceBuilder;
